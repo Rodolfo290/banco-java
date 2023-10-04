41446cfb3ea8a8664dcc574fb2de2f14a220374e
Rodolfo Oliveira De Melo <rodolfo.oli29@gmail.com>
2023-10-03 23:18:06 -0300

    código finalizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+import java.util.Locale;
 public class ContaTerminal {
+   
     public static void main(String[] args) throws Exception {
-        System.out.println("Meu Banco!");
+     Scanner scanner = new Scanner(System.in).useLocale(Locale.US);        
+       
+        int numero = 1021;
+        // String agencia = "";
+        String nomeCliente = "Rodolfo Oliveira";
+        double saldo = 237.48;
+   
+        System.out.println("Por favor, digite o número da Agência !");
+        String agencia = scanner.nextLine();
+
+        System.out.printf("Olá " + nomeCliente + "," + " obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+       
+
+
+scanner.close();
     }
 }
